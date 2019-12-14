60c9ba02362ba115c724f1647fd83971c4ad02f6
archetype2142 <malhotraritwick2011@gmail.com>
2019-12-14 15:34:08 +0100

    fixed email

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'piotr-putylo',
-  :password => 'wezsiejuz1',
-  :domain => 'two-do-net.herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 465,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
-
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'localhost' }
-  config.action_mailer.asset_host = "http://" + 'localhost'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => 'piotr-putylo',
+    :password => 'wezsiejuz1',
+    :domain => 'two-do-net.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
